b4a0247eb70fe229daddf5ee77ecfed5f5e05743
AbolghasemTaziki <a.taziki70@gmail.com>
2023-12-14 15:07:15 +0330

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -10,31 +10,30 @@ import AppLayout from "./ui/AppLayout";
 import OwnerDashboard from "./pages/OwnerDashboard";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
+import { DarkModeProvider } from "./context/DarkModeContext";
 const queryClient = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
-   <div className="">
-   <Routes>
-     
-     <Route path="/auth" element={<Auth />} />
-     <Route path="/complete-profile" element={<CompleteProfile />} />
-     <Route path="/owner" element={<AppLayout />}>
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <div className="">
+          <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/complete-profile" element={<CompleteProfile />} />
+            <Route path="/owner" element={<AppLayout />}>
+              <Route index element={<Navigate to="dashboard" replace />} />
+              <Route path="dashboard" element={<OwnerDashboard />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="projects/:id" element={<Project />} />
+            </Route>
 
-     <Route index element={<Navigate to="dashboard" replace />} />
-     <Route path="dashboard" element={<OwnerDashboard />} />
-     <Route path="projects" element={<Projects />} />
-     <Route path="projects/:id" element={<Project />} />
-     </Route>
-
-
-
-     <Route path="/" element={<Home />} />
-       <Route path="*" element={<NotFound />} />
-   </Routes>
-   </div>
-    </QueryClientProvider>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
