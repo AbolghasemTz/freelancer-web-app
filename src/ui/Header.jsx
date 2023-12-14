133b9901bbb4bbf5a8f8d5b5ed4b1c5a14e8e165
AbolghasemTaziki <a.taziki70@gmail.com>
2023-12-14 12:39:31 +0330

    user avatar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import useUser from '../feature/authentication/useUser'
+import HeaderMenu from './HeaderMenu';
+import UserAvatar from '../feature/authentication/UserAvatar';
 
 function Header() {
-    const {data} = useUser();
-  
+  const {user,isLoading} = useUser();
   return (
-    <div className="bg-secondary-0 py-4 px-8 ">app header</div>
+    <div className="bg-secondary-0 py-4 px-8 border-secondary-200">
+      <div className={`container max-w-screen-lg flex items-center justify-end gap-x-8
+      ${isLoading ? "blur-sm opacity-50" :""}
+      `}>
+        <HeaderMenu />
+        <UserAvatar />
+      </div>
+    </div>
   )
 }
 
